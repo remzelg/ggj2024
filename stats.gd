a34c2885eb4e8b839e00245332d3eb0bf7517c4d
thegrumpybuffalo <thegrumpybuffalo@gmail.com>
2024-01-27 13:17:30 -0500

    resources

--- stats.gd
+++ stats.gd
@@ -1,6 +1,6 @@
-extends Resource
+class_name Stats extends Resource
 
 @export var wit : int
 @export var obs : int
 @export var pride : int
-@export_range(1,100) var delivery
+@export_range(1,100) var delivery : int
