94581a1a13c38d40dbafba2bea8fb8b49529a264
Harrison <hcstebbins@gmail.com>
2024-01-26 23:19:10 -0500

    audio game setting

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -1,29 +1,27 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_settings_button_pressed():
-	$SplashMenu.set_visible(true)
-	$SettingsMenu.visible = true # Replace with function body.
-
+	$SplashMenu.set_visible(false)
+	$SettingsMenu.set_visible(true)
 
 func _on_back_button_pressed():
 	$SettingsMenu.visible = false
 	$SplashMenu.visible = true # Replace with function body.
 
-
 func _on_quit_button_pressed():
 	get_tree().quit() # Replace with function body.
 
-
 func _on_start_button_pressed():
 	pass
+	
+func _on_audio_slider_value_changed(value):
+	GameSettings.audioLevel = value
+	
