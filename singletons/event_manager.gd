0ab4e97a13f7e0b4d37d4391269d3b4bfdd7bbe9
Remy Carr <remycarr@gmail.com>
2024-01-27 22:57:36 -0500

    Add code for updating stats from dialogue

--- singletons/event_manager.gd
+++ singletons/event_manager.gd
@@ -1,12 +1,11 @@
 extends Node
 
-var stored_changes = {}
+var BLANK_CHANGES = { "wit": 0, "pride": 0, "obs": 0, "delivery": 50, "jokes": [] }
+var stored_changes = BLANK_CHANGES
 
 func get_event_effects():
 	return stored_changes
 
-func store_event_effects(changes):
-	stored_changes = {} # wipe previous data
-	
-	stored_changes = changes
-
+func store_event_effects(wit=0, pride=0, obs=0, delivery=0, jokes=[]):
+	# Override previous event changes
+	stored_changes = { "wit": wit, "pride": pride, "obs": obs, "delivery": delivery, "jokes": jokes }
